99c81446328971767e1ac7fae4da4a9714a38b84
Dinh Nguyen <dinhn4@gmail.com>
2023-02-09 22:02:46 -0800

    Update 0001_initial.py

--- webpush/migrations/0001_initial.py
+++ webpush/migrations/0001_initial.py
@@ -17,18 +17,4 @@ class Migration(SchemaMigration):
             ('name', models.CharField(unique=True, max_length=255)),
         ))
         db.send_create_signal('webpush', ['Group'])
-        db.create_table(u'PushInformation', (
-            ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-            ('group', models.ForeignKey(related_name='webpush_info', blank=True, to=orm['webpush.Group'], null=True)),
-            ('subscription', models.ForeignKey(related_name='webpush_info', to=orm['webpush.SubscriptionInfo'])),
-            ('user', models.ForeignKey(related_name='webpush_info', blank=True, to=orm['authentication.User'], null=True)),
-        ))
-        db.send_create_signal('webpush', ['PushInformation'])
-        db.create_table(u'SubscriptionInfo', (
-            ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-            ('browser', models.CharField(max_length=100)),
-            ('endpoint', models.URLField(max_length=255)),
-            ('auth', models.CharField(max_length=100)),
-            ('p256dh', models.CharField(max_length=100)),
-        ))
-        db.send_create_signal('webpush', ['SubscriptionInfo'])
+
