665a0b69e7fc82d5e7173ba1fa3b7ed24be36a14
Dinh Nguyen <dinhn4@gmail.com>
2023-02-09 21:47:05 -0800

    Update 0001_initial.py

--- webpush/migrations/0001_initial.py
+++ webpush/migrations/0001_initial.py
@@ -8,43 +8,33 @@ from django.conf import settings
 
 class Migration(SchemaMigration):
 
-    dependencies = [
-        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-    ]
-
-    operations = [
-        migrations.CreateModel(
-            name='Group',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('name', models.CharField(unique=True, max_length=255)),
-            ],
-        ),
-        migrations.CreateModel(
-            name='PushInformation',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('group', models.ForeignKey(related_name='webpush_info', blank=True, to='webpush.Group', null=True)),
-            ],
-        ),
-        migrations.CreateModel(
-            name='SubscriptionInfo',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('browser', models.CharField(max_length=100)),
-                ('endpoint', models.URLField(max_length=255)),
-                ('auth', models.CharField(max_length=100)),
-                ('p256dh', models.CharField(max_length=100)),
-            ],
-        ),
-        migrations.AddField(
-            model_name='pushinformation',
-            name='subscription',
-            field=models.ForeignKey(related_name='webpush_info', to='webpush.SubscriptionInfo'),
-        ),
-        migrations.AddField(
-            model_name='pushinformation',
-            name='user',
-            field=models.ForeignKey(related_name='webpush_info', blank=True, to=settings.AUTH_USER_MODEL, null=True),
-        ),
-    ]
+   # dependencies = [
+   #    migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+   # ]
+    def forwards(self, orm):
+        db.create_table(u'Group', (
+            ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+            ('name', models.CharField(unique=True, max_length=255)),
+        ))
+        db.create_table(u'PushInformation', (
+            ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+            ('group', models.ForeignKey(related_name='webpush_info', blank=True, to='webpush.Group', null=True)),
+        ))
+        db.create_table(u'SubscriptionInfo', (
+            ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+            ('browser', models.CharField(max_length=100)),
+            ('endpoint', models.URLField(max_length=255)),
+            ('auth', models.CharField(max_length=100)),
+            ('p256dh', models.CharField(max_length=100)),
+        ))
+        db.create_table(u'pushinformation', (
+            ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+            ('browser', models.CharField(max_length=100)),
+            ('endpoint', models.URLField(max_length=255)),
+            ('auth', models.CharField(max_length=100)),
+            ('p256dh', models.CharField(max_length=100)),
+            ('subscription', models.ForeignKey(related_name='webpush_info', to='webpush.SubscriptionInfo')),
+            ('user', models.ForeignKey(related_name='webpush_info', blank=True, to=orm['authentication.User'], null=True)),
+        ))
+    def backwards(self, orm):
+        
